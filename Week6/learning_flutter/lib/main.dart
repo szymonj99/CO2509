31d4d7f0ed3d5a2c0c2f9e8419b59af7757dfbdf
szymonj99 <szymon.j@hotmail.com>
2020-11-24 02:32:41 +0000

    Finish exercise

--- Week6/learning_flutter/lib/main.dart
+++ Week6/learning_flutter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String GradeOutput (int grade)
+String gradeOutput (int grade)
 {
   if (grade >= 70)
     {
@@ -12,20 +12,39 @@ String GradeOutput (int grade)
     }
 }
 
-String GetTitle()
+String getTitle()
 {
   var year = 2020;
   var module = "CO2509";
   return "Hello World! " + module + " " + year.toString();
 }
 
+class Student
+{
+  String name;
+  int grade;
+
+  Student(String name, int grade)
+  {
+    this.name = name;
+    this.grade = grade;
+  }
+  void study()
+  {
+    print ("*************************");
+    print("${this.name} is studying.");
+  }
+}
+
+
+
 void main()
 {
-  print(GetTitle());
+  print(getTitle());
   var myGrades = [1, 56, 76, 23];
   for (var item in myGrades)
   {
-      print(GradeOutput(item));
+      print(gradeOutput(item));
   }
 
   var image =
@@ -35,4 +54,19 @@ void main()
   };
 
   print (image["url"]);
+
+  try
+  {
+    print (20 / 0);
+  }
+  on Exception
+  {
+    print("Can't divide by 0.");
+  }
+
+  Student student = Student("Test", 20);
+
+  student.name = "Simon";
+  student.grade = 100;
+  student.study();
 }
